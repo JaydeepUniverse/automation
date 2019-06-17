646746c0b324021fb5db04da461bfd9055320451
JaydeepUniverse <ecjaydeepsoni@gmail.com>
2019-06-17 11:55:09 +0000

    changed credentials in aws config script and in previous commit also added install command for boto3 library for aws python sdk

--- linux_aws_python_sdk_configuration.sh
+++ linux_aws_python_sdk_configuration.sh
@@ -3,7 +3,7 @@
 mkdir ~/.aws
 cat <<EOF > ~/.aws/credentials
 [default]
-aws_access_key_id = AKIAIN7M6NEO72556F6Q
-aws_secret_access_key = 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
+aws_access_key_id = xxxxxxxxxxxxxxxxxxx
+aws_secret_access_key = yyyyyyyyyyyyyyyyyyyyyy
 region=ap-south-1
 EOF
