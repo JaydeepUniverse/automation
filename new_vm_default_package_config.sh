00e40083159977731fe6f747fe34c4e8c7bc44d0
jaydeepuniverse <root@Test.sw0eylxhdw5udpukz1wgyhd4mb.hx.internal.cloudapp.net>
2019-06-17 06:49:07 +0000

    added python and pip installation script for centos and amazon ec2

--- new_vm_default_package_config.sh
+++ new_vm_default_package_config.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+yum update -y
+
 ############### KUBECTL INSTALLATION ###########################
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo  
 [kubernetes]  
@@ -26,3 +28,42 @@ rpm -ivh $cliVersion
 yum install -y git
 git config --global user.mail "ecjaydeepsoni@gmail.com"
 git config --global user.name "jaydeepuniverse"
+
+################ PYTHON and PIP INSTALLATION ##########################
+install_python3_in_centos()
+{
+        yum install gcc openssl-devel bzip2-devel  libffi-devel -y
+        cd /usr/src
+        wget https://www.python.org/ftp/python/3.7.0/Python-3.7.0.tgz
+        tar xzf Python-3.7.0.tgz
+        cd Python-3.7.0
+        ./configure --enable-optimizations
+        make altinstall
+        rm -f /usr/src/Python-3.7.0.tgz
+        python3.7 -V
+        pip3.7 install ipython
+}
+
+install_python3_in_amazonec2()
+{
+        yum install python3 -y
+        python3 --version
+        cd /usr/src
+        curl -O https://bootstrap.pypa.io/get-pip.py
+        python3 get-pip.py --user
+        export PATH=~/.local/bin:$PATH
+        source ~/.bash_profile
+        pip3 --version
+        pip3 install ipython
+}
+
+os_type=`head -n1 /etc/system-release`
+if [[ $os_type == "CentOS"* ]]; then
+        echo "CentOS"
+        install_python3_in_centos
+elif [[ $os_type == "Amazon"* ]]; then
+        echo "Amazon EC2"
+        install_python3_in_amazonec2
+else
+        echo "New OS Type"
+fi
