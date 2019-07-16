d894845f2f22dfba147be9567e47b04d6d8adc68
JaydeepUniverse <ecjaydeepsoni@gmail.com>
2019-07-16 11:12:39 +0000

    added lines to remove index.html and az rpm in AZ CLI script after installation

--- linux_new_vm_default_package_azure_cli.sh
+++ linux_new_vm_default_package_azure_cli.sh
@@ -6,6 +6,8 @@ yum install -y python2-virtualenv
 ############### AZ CLI INSTALLATION ###########################
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+wget https://packages.microsoft.com/yumrepos/azure-cli/
 cliVersion=`tail -n3 index.html | head -n1 | cut -d "\"" -f 2`
 wget https://packages.microsoft.com/yumrepos/azure-cli/$cliVersion
 rpm -ivh $cliVersion
+rm -rf index.html $cliVersion
